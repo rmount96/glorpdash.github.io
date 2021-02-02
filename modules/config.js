d62740f4b59e2bd60b1972075eed1c0c027bfb19
Ryan Mount <72673023+rmount96@users.noreply.github.com>
2021-02-02 16:07:51 -0600

    Add files via upload

---
+++ modules/config.js
@@ -0,0 +1,8 @@
+let corsFix = "http://ec2-18-219-200-38.us-east-2.compute.amazonaws.com:8080/";
+export const URLs = {
+    backgroundImage: `https://picsum.photos/id/`,
+    quote: `https://quote-garden.herokuapp.com/api/v2/quotes/random`,
+    news:
+        corsFix +
+        "http://newsapi.org/v2/top-headlines?country=us&pageSize=3&apiKey=5ed46a162aee4178a84fb1602ae3e70c",
+};
